2178eb92c32891697e9dbf7c21ebd57657142690
Kyle Awayan <kyle@awayan.com>
2024-03-20 18:13:36 -0700

    we actually don't need to send first/second stage for status packets

--- experiments/pro_dj_link.py
+++ experiments/pro_dj_link.py
@@ -1,5 +1,4 @@
 import socket
-import time
 
 pro_dj_link_begin_bytes = [0x51, 0x73, 0x70, 0x74, 0x31, 0x57, 0x6D, 0x4A, 0x4F, 0x4C]
 
@@ -32,21 +31,6 @@ class ProDjLink:
         s.sendto(bytearray(packet_bytes), (ip, port))
         s.close()
 
-    def connect(self):
-        # Send first-stage channel number claim 3 times
-        for i in range(3):
-            self.send_first_stage(i + 1)
-            time.sleep(0.03)
-
-        # Send second-stage channel number claim
-        # d we need to send
-        d_needed = [0x11, 0x12, 0x29, 0x2A, 0x2B, 0x2C]
-
-        for i in range(6):
-            for d in d_needed:
-                self.send_second_stage(d, i + 1)
-                time.sleep(0.03)
-
     @staticmethod
     def encode_device_name(device_name):
         device_name_bytes = device_name.encode("utf-8")
@@ -72,39 +56,6 @@ class ProDjLink:
     @staticmethod
     def encode_ip_address(ip_address):
         return list(map(int, ip_address.split(".")))
-
-    def send_first_stage(self, n):
-        packet = (
-            pro_dj_link_begin_bytes
-            + [0x00, 0x00]
-            + self.encode_device_name("rekordbox")
-        )
-        packet += [0x01, 0x03, 0x00, 0x2C]
-        packet += [n]
-        packet += [0x01]
-        packet += self.encode_mac_address(self.interface_mac_address)
-        self.broadcast_packet(packet, 50000)
-
-    def send_second_stage(self, d, n):
-        packet = (
-            pro_dj_link_begin_bytes
-            + [0x02, 0x00]
-            + self.encode_device_name("rekordbox")
-        )
-        # other stuff + Length of bit idk
-        packet += [0x01, 0x03, 0x00, 0x32]
-        # ip address
-        packet += self.encode_ip_address(self.interface_ip)
-        # mac address
-        packet += self.encode_mac_address(self.interface_mac_address)
-        # d
-        packet += [d]
-        # n
-        packet += [n]
-        # idk
-        packet += [0x04, 0x01]
-
-        self.broadcast_packet(packet, 50000)
 
     def send_cdj(self):
         # If self.opus_ip is None, throw error
--- experiments/sub.py
+++ experiments/sub.py
@@ -2,14 +2,12 @@ from pro_dj_link import ProDjLink
 import time
 import subprocess
 
-interface_ip = "192.168.112.213"  # macbook IP address
+interface_ip = "192.168.112.235"  # macbook IP address
 interface_mac_address = "3c:18:a0:99:7b:20"
 this_device_name = "macbook"
 
 link = ProDjLink(interface_ip, interface_mac_address, this_device_name)
 link.opus_ip = "192.168.112.180"
-
-link.connect()
 
 dumbCounter = -3
 
