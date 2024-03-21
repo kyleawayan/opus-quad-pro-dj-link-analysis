2702db37ad8b577c17d3c399f918e28886da0386
Kyle Awayan <kyle@awayan.com>
2024-03-20 17:14:57 -0700

    computer name

--- experiments/sub.py
+++ experiments/sub.py
@@ -2,9 +2,9 @@ from pro_dj_link import ProDjLink
 import time
 import subprocess
 
-interface_ip = "192.168.112.213"  # yukikaM3 IP address
+interface_ip = "192.168.112.213"  # macbook IP address
 interface_mac_address = "3c:18:a0:99:7b:20"
-this_device_name = "yukikaM3"
+this_device_name = "macbook"
 
 link = ProDjLink(interface_ip, interface_mac_address, this_device_name)
 link.opus_ip = "192.168.112.180"
